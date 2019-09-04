82f81d8f9baddaa4f5b4cc43be6fbe2fd2590555
HemanthNewage <hemanthkumar.bv@newage-global.com>
2019-09-04 17:18:51 +0530

    partial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'deals', component: MainpageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
