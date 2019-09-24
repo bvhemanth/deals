46b23a10497652769e356066fbdc43efe94eb831
HemanthNewage <hemanthkumar.bv@newage-global.com>
2019-09-24 18:43:07 +0530

    partial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainpageComponent
+    MainpageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
